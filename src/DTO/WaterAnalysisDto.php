c3de59b36033ad86516878aecdaccc9586553818
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-12-29 17:09:48 +0100

    refactor: adapt dto with use of enums

--- src/DTO/WaterAnalysisDto.php
+++ src/DTO/WaterAnalysisDto.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\DTO;
 
+use App\Enums\WaterParameter;
+
 final class WaterAnalysisDto
 {
     public function __construct(
-        public readonly string $parameter,
+        public readonly WaterParameter $parameter,
         public readonly ?float $value,
         public readonly ?string $unit,
         public readonly \DateTimeImmutable $date
